33ce021b5e4be72f034e52c4b83e0290c2f6d8ff
AnastasiiaNotych <notychwork@gmail.com>
2025-11-30 20:51:45 +0100

    fix filtr method

--- js/task-2.js
+++ js/task-2.js
@@ -10,17 +10,17 @@ class Storage {
     this.#items.push(newItem);
   }
   removeItem(itemToRemove) {
-    this.#items.filter((item) => item !== itemToRemove);
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem("Droid");
+storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-storage.removeItem("Prolonger");
+storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-storage.removeItem("Scaner");
+storage.removeItem('Scaner');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
